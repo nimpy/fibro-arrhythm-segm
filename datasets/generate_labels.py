f40da5c1e9be41102ff700b1b5dd78ff55fcaae2
niaki <nina.zizakic@ugent.be>
2021-03-24 18:04:56 +0100

    Generalise (and clean up) dataset generation

--- datasets/generate_labels.py
+++ datasets/generate_labels.py
@@ -1,10 +1,8 @@
 import os
 import pickle
 import numpy as np
-import imageio
 import matplotlib.pyplot as plt
 import argparse
-from sklearn.preprocessing import normalize
 
 
 parser = argparse.ArgumentParser()
@@ -13,7 +11,7 @@ parser.add_argument('--data_dir_arrhythm', default='/scratch/fibro_arrhythm_data
 parser.add_argument('--data_dir_nonarrhythm', default='/scratch/fibro_arrhythm_data/OriginalTextures/OriginalNonArrhythmogenic',
                     help="Directory containing the non-arrhythmogenic textures")
 
-parser.add_argument('--goal_data_dir', default='/scratch/fibro_arrhythm_data/ds210320',
+parser.add_argument('--goal_data_dir', default='/scratch/fibro_arrhythm_data/ds210324',
                     help="Directory where the dataset ready for training will be stored")
 
 parser.add_argument('--zfill_param', default=4,
@@ -25,123 +23,118 @@ def gaus(x, m, s):
     return 1 / (np.sqrt(2 * np.pi * s ** 2)) * np.exp(-(x - m) ** 2 / (2 * s ** 2))
 
 
-def gaus_2D(x, y, s):
-    xx, yy = np.meshgrid(np.arange(254), np.arange(254))
-    return gaus(xx, x, s) * gaus(yy, y, s)
+def gaus_2D(x, y, gaus_size, image_size):
+    xx, yy = np.meshgrid(np.arange(image_size), np.arange(image_size))
+    return gaus(xx, x, gaus_size) * gaus(yy, y, gaus_size)
+
+
+def generate_dataset(base_dir, goal_dir, dataset_name, arrhythm, visualise, saving=False):
+    assert not (visualise and saving), "This is a bad idea."
+
+    texture_dirs = os.listdir(base_dir)
+
+    for texture_dir in texture_dirs:
+
+        # texture
+        texture = np.load(os.path.join(base_dir, texture_dir, "texture.npy"))
+        texture = texture[1:-1, 1:-1]
+        texture = texture - 1
+        assert np.min(texture) - 0.0 < 0.00001, "The textures have some unexpected values"
+        assert np.max(texture) - 1.0 < 0.00001, "The textures have some unexpected values"
+
+        texture_ID = texture_dir[3:]
+        # texture_ID = texture_ID.zfill(args.zfill_param)
+
+        print(dataset_name + '_' + texture_ID)
+        
+        # if texture_ID == '944_0':  # problematic
+        #     print()
+
+        texture_filename = 'texture_' + dataset_name + '_' + texture_ID + '.npy'
+        # print(os.path.join(args.goal_data_dir, 'textures', texture_filename))
+
+        label_image = np.zeros_like(texture)
+
+        if arrhythm:
+
+            # cores
+            with open(os.path.join(base_dir, texture_dir, "cores.pickle"), 'rb') as fp:
+                cores = pickle.load(fp)
+
+                if visualise:
+                    plt.imshow(texture[1:-1, 1:-1])  # TODO (i.e. to ask) why crop? why is there a border of zeros?
+                    for core in cores:
+                        plt.plot(core[0], core[1], "ro")
+                        plt.plot(np.mean(core[0]), np.mean(core[1]), "bo")
+
+                for core in cores:
+                    assert len(core) == 2, "There is more than 2 coordinates?"
+
+                    core_center_x = np.mean(core[0])
+                    core_center_y = np.mean(core[1])
+                    core_center = np.array(core_center_x, core_center_y)
+
+                    distances_from_core = []
+                    for i in range(len(core[0])):
+                        distance_from_core = np.linalg.norm(np.array(core[0][i], core[1][i]) - core_center)
+                        distances_from_core.append(distance_from_core)
+
+                    max_distance_from_core = np.max(np.array(distances_from_core))
+
+                    assert label_image.shape[0] == label_image.shape[1], \
+                        "Generating non-square label images not (yet?) supported..."
+                    gaussian_core = gaus_2D(core_center_x, core_center_y, max_distance_from_core, label_image.shape[0])
+
+                    gaussian_core_normalised = (gaussian_core - gaussian_core.min()) / (
+                                gaussian_core.max() - gaussian_core.min())
+
+                    gaussian_core_normalised[gaussian_core_normalised < 0.01] = 0  # set to 1 to visualise the thresholding
+
+                    label_image += gaussian_core_normalised
+
+                label_image[label_image > 1.0] = 1  # since these gaussian balls were additive, they need to be cut off at 1
+
+                if visualise:
+                    plt.imshow(label_image, cmap='gray')
+                    plt.show()
+
+        label_filename = 'label_' + dataset_name + '_' + texture_ID + '.npy'
+        # print(os.path.join(args.goal_data_dir, 'labels', label_filename))
+
+        assert np.min(label_image) > -0.00001, "The label images have some unexpected values"
+        assert np.max(label_image) < 1.00001, "The label images have some unexpected values"
+
+        # print(np.min(label_image), np.max(texture))
+        # print(np.min(label_image), np.max(label_image))
+
+        if saving:
+            with open(os.path.join(goal_dir, 'textures', texture_filename), 'wb') as f:
+                np.save(f, texture)
+            with open(os.path.join(goal_dir, 'labels', label_filename), 'wb') as f:
+                np.save(f, label_image)
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
 
-    # TODO convert both texture and label image array types to float32
+    # TODO convert both texture and label image array types to float32 (now it's done when loading the dataset)
 
-    # arrhythmogenic  # TODO put this into a function
+    saving = True
 
-    base_dir = args.data_dir_arrhythm
-    texture_dirs = os.listdir(base_dir)
+    generate_dataset('/scratch/fibro_arrhythm_data/OriginalTextures/OriginalArrhythmogenic', args.goal_data_dir, "orig", arrhythm=True, visualise=False, saving=saving)
+    generate_dataset('/scratch/fibro_arrhythm_data/OriginalTextures/OriginalNonArrhythmogenic', args.goal_data_dir, "orig", arrhythm=False, visualise=False, saving=saving)
 
-    for texture_dir in texture_dirs:
+    generate_dataset('/scratch/fibro_arrhythm_data/ExtraData/Dataset_ap6_yes', args.goal_data_dir, "dsap6", arrhythm=True, visualise=False, saving=saving)
+    generate_dataset('/scratch/fibro_arrhythm_data/ExtraData/Dataset_ap6_no', args.goal_data_dir, "dsap6", arrhythm=False, visualise=False, saving=saving)
 
-        # texture
-        texture = np.load(os.path.join(base_dir, texture_dir, "texture.npy"))
-        texture = texture[1:-1, 1:-1]
-        texture = texture - 1
-        assert np.min(texture) - 0.0 < 0.00001, "The textures have some unexpected values"
-        assert np.max(texture) - 1.0 < 0.00001, "The textures have some unexpected values"
+    generate_dataset('/scratch/fibro_arrhythm_data/ExtraData/Dataset_ap24_yes', args.goal_data_dir, "dsap24", arrhythm=True, visualise=False, saving=saving)
+    generate_dataset('/scratch/fibro_arrhythm_data/ExtraData/Dataset_ap24_no', args.goal_data_dir, "dsap24", arrhythm=False, visualise=False, saving=saving)
 
-        texture_ID = texture_dir[3:]
-        texture_ID = texture_ID.zfill(args.zfill_param)
+    generate_dataset('/scratch/fibro_arrhythm_data/ExtraData/Dataset_other/Dataset_ap6_31_yes', args.goal_data_dir, "dsoap631", arrhythm=True, visualise=False, saving=saving)
+    generate_dataset('/scratch/fibro_arrhythm_data/ExtraData/Dataset_other/Dataset_ap6_31_no', args.goal_data_dir, "dsoap631", arrhythm=False, visualise=False, saving=saving)
 
-        print(texture_ID)
+    generate_dataset('/scratch/fibro_arrhythm_data/ExtraData/Dataset_other/Dataset_ap6_35_yes', args.goal_data_dir, "dsoap635", arrhythm=True, visualise=False, saving=saving)
+    generate_dataset('/scratch/fibro_arrhythm_data/ExtraData/Dataset_other/Dataset_ap6_35_no', args.goal_data_dir, "dsoap635", arrhythm=False, visualise=False, saving=saving)
 
-        texture_filename = 'texture_' + texture_ID + '.npy'
-        print(os.path.join(args.goal_data_dir, 'textures', texture_filename))
+    # total number of data samples: 15637
 
-        # saving
-        # with open(os.path.join(args.goal_data_dir, 'textures', texture_filename), 'wb') as f:
-        #     np.save(f, texture)
-
-
-        # cores
-        with open(os.path.join(base_dir, texture_dir, "cores.pickle"), 'rb') as fp:
-            cores = pickle.load(fp)
-
-            # plt.imshow(texture[1:-1, 1:-1])  # TODO (i.e. to ask) why crop? why is there a border of zeros?
-            # for core in cores:
-            #     plt.plot(core[0], core[1], "ro")
-            #     plt.plot(np.mean(core[0]), np.mean(core[1]), "wo")
-
-            label_image = np.zeros_like(texture)
-
-            for core in cores:
-                assert len(core) == 2, "There is more than 2 coordinates?"
-
-                core_center_x = np.mean(core[0])
-                core_center_y = np.mean(core[1])
-                core_center = np.array(core_center_x, core_center_y)
-
-                distances_from_core = []
-                for i in range(len(core[0])):
-                    distance_from_core = np.linalg.norm(np.array(core[0][i], core[1][i]) - core_center)
-                    distances_from_core.append(distance_from_core)
-
-                max_distance_from_core = np.max(np.array(distances_from_core))
-
-                gaussian_core = gaus_2D(core_center_x, core_center_y, max_distance_from_core)
-
-                gaussian_core_normalised = (gaussian_core - gaussian_core.min()) / (gaussian_core.max() - gaussian_core.min())
-
-                gaussian_core_normalised[gaussian_core_normalised < 0.01] = 0  # set to 1 to visualise the thresholding
-
-                label_image += gaussian_core_normalised
-
-            label_image[label_image > 1.0] = 1  # since these gaussian balls were additive, they need to be cut off at 1
-            # plt.imshow(label_image, cmap='gray')
-            # plt.show()
-
-            label_filename = 'label_' + texture_ID + '.npy'
-            print(os.path.join(args.goal_data_dir, 'labels', label_filename))
-
-            print(np.min(texture), np.max(texture))
-            print(np.min(label_image), np.max(label_image))
-
-            # saving
-            # with open(os.path.join(args.goal_data_dir, 'labels', label_filename), 'wb') as f:
-            #     np.save(f, label_image)
-
-
-    # non-arrhythmogenic
-
-    base_dir = args.data_dir_nonarrhythm
-    texture_dirs = os.listdir(base_dir)
-
-    for texture_dir in texture_dirs:
-
-        # texture
-        texture = np.load(os.path.join(base_dir, texture_dir, "texture.npy"))
-        texture = texture[1:-1, 1:-1]
-        texture = texture - 1
-        assert np.min(texture) - 0.0 < 0.00001, "The textures have some unexpected values"
-        assert np.max(texture) - 1.0 < 0.00001, "The textures have some unexpected values"
-
-        texture_ID = texture_dir[3:]
-        texture_ID = texture_ID.zfill(args.zfill_param)
-
-        print(texture_ID)
-
-        texture_filename = 'texture_' + texture_ID + '.npy'
-        print(os.path.join(args.goal_data_dir, 'textures', texture_filename))
-
-        label_image = np.zeros_like(texture)
-
-        label_filename = 'label_' + texture_ID + '.npy'
-        print(os.path.join(args.goal_data_dir, 'labels', label_filename))
-
-        print(np.min(texture), np.max(texture))
-        print(np.min(label_image), np.max(label_image))
-
-        # saving
-        # with open(os.path.join(args.goal_data_dir, 'textures', texture_filename), 'wb') as f:
-        #     np.save(f, texture)
-        # with open(os.path.join(args.goal_data_dir, 'labels', label_filename), 'wb') as f:
-        #     np.save(f, label_image)
